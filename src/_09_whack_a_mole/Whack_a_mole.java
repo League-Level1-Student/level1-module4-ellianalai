6a8cbf2aadf20a388ff656670fa8896fafe7bb65
league <league@localhost>
2024-02-09 18:28:49 -0800

    Working on slot machine

--- src/_09_whack_a_mole/Whack_a_mole.java
+++ src/_09_whack_a_mole/Whack_a_mole.java
@@ -1,11 +1,15 @@
 package _09_whack_a_mole;
 
+import java.awt.Button;
+import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Whack_a_mole implements ActionListener{
@@ -13,31 +17,75 @@ public class Whack_a_mole implements ActionListener{
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button;
+	Date timeAtStart = new Date();
 	Random ran = new Random();
+	int score = 0;
 
 	void whack() {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		drawButtons(ran);
+		drawButtons();
 		frame.setSize(275,315);
 	}
-	
-	void drawButtons(Random rand) {
+
+	void drawButtons() {
+		int r = ran.nextInt(24);
 		for(int i = 0; i<24; i++) {
-			button = new JButton();
-			panel.add(button);
-			
-		}
-		rand.nextInt(24);
-		
-		
-		
+			JButton button2 = new JButton();
+			button2.addActionListener(this);
+			panel.add(button2);
+			if(i==r) {
+				button2.setText("Mole");	
+				button = button2;
+			}
+
+		}	
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		JButton pressed = (JButton) e.getSource();
+
+		if(pressed == button) {
+			score+=1;
+			panel.removeAll();
+			drawButtons();
+		}
+		if(pressed != button) {
+			speak("Miss");
+		}
 		
-		
+		if(score==10) {
+			endGame(timeAtStart, 10);
+		}
+
 	}
+
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+		Date timeAtEnd = new Date();
+		JOptionPane.showMessageDialog(null, "Your whack rate is "
+				+ ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+				+ " moles per second.");
+	}
+	
+    static void speak(String words) {
+        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+                    + words + "');\"";
+            try {
+                Runtime.getRuntime().exec( cmd ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Runtime.getRuntime().exec( "say " + words ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/_10_slot_machine/SlotMachine.java
+++ src/_10_slot_machine/SlotMachine.java
@@ -14,27 +14,32 @@ public class SlotMachine {
 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
+	JPanel panel2 = new JPanel();
+	JPanel panel3 = new JPanel();
 	JButton button = new JButton();
-	
-	void slot() throws MalformedURLException {
+
+	void slot() {
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		panel.add(createLabelImage("bannana.png"));
+		panel.add(createLabelImage("banana.png"));
+		panel.add(createLabelImage("diamond.png"));
+		panel.add(createLabelImage("cherry.png"));
+		
 		frame.pack();
 	}
-	
-	
-	
-	private JLabel createLabelImage(String fileName) throws MalformedURLException{
-        URL imageURL = getClass().getResource(fileName);
-	if (imageURL == null){
-		System.err.println("Could not find image " + fileName);
-		return new JLabel();
+
+
+
+	private JLabel createLabelImage(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		if (imageURL == null){
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+		Icon icon = new ImageIcon(imageURL);
+		JLabel imageLabel = new JLabel(icon);
+		return imageLabel;
 	}
-	Icon icon = new ImageIcon(imageURL);
-	JLabel imageLabel = new JLabel(icon);
-	return imageLabel;
-}
-    
+
 }
